830cdeae0847fbfd2bba631552aa8b5083dc5477
IsabelleOrmo <bellecrenonini@gmail.com>
2023-03-01 08:32:44 -0300

    while do for break e continue

---
+++ xampp/htdocs/aulas/whileDo.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php
+  /*$n=1;
+  $tabuada=7;
+
+while($n<=10){
+  echo $n . " x " . $tabuada . " = " . ($n * $tabuada) . "<br>";
+  $n++;
+}
+$n=1;
+$tabuada=7;
+
+do{
+  echo $n . " x " . $tabuada . " = " . ($n * $tabuada) . "<br>";
+  $n++;
+} 
+while($n<=10);*/
+
+/*$tabuada = 7;
+
+for($n=1;$n<=10; $n++){
+  echo $n . " x " . $tabuada . " = ". ($n * $tabuada) . "<br>";
+}*/
+
+/*$cores = array("Branco", "Verde", "Vermelho");
+
+foreach($cores as $cor){
+  echo $cor . "<br>";
+}
+
+$valores = array(55.00, 10.99, 20.00);
+$total = 0;
+
+foreach($valores as $valor){
+  $total = $total + $valor;
+}*/
+
+/*$n=1;
+$tabuada=7;
+
+do{
+  echo $n . " x " . $tabuada . " = " . ($n * $tabuada) . "<br>";
+  if($n==7)break; //se trocarmos por continue ele irá pular a linha
+  $n++;
+}while($n<=10);*/
+
+                                                                                                                                                                                                                                                                                                                                                                  
+
+
+?>
+</  </body>
+</html>
